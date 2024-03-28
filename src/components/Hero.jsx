0ad788b9016bf3c4dec04d6cfe768ddfc6d89317
mijan123 <msmizans@gmail.com>
2024-03-28 23:55:49 +0545

    button for cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,25 @@ import { ComputersCanvas } from './canvas'
 import { styles } from '../styles'
 
 const Hero = () => {
+  const downloadCV = () => {
+    window.open('https://drive.google.com/uc?export=download&id=1Qbrt4sBlSApYrwmuss1lWKkIBgMzGQ5H', '_blank');
+  }
   return (
     <section className='relative w-full h-screen mx-auto'>
-        <div className={` ${styles.paddingX} absolute inset-0 top-[120px] max-w-8xl mx-auto flex flex-row items-start gap-5 mb-20`}>
+        <div className={` ${styles.paddingX} absolute inset-0 top-[120px] max-w-8xl mx-auto flex flex-row items-start gap-5 mb-20 z-10`}>
           <div className='flex flex-col justify-center items-center mt-5'>
             <div className="w-5 h-5 rounded-full bg-neon"></div>
             <div className='w-1 sm:h-80 h-40 neon-gradient'></div>
           </div>
           <div>
             <h1 className= { `text-white {${styles.heroHeadText}`}>Namaste , I am <span className={`sm:text-[45px] text-[24px] text-neon  font-bold`}>Mijan Shrestha</span></h1>
-            <p className={`${ styles.heroSubText} text-white `}> As a frontend web developer, I specialize in crafting engaging websites.</p>
+            <p className={`${ styles.heroSubText} text-white mb-3 `}> As a frontend web developer, I specialize in crafting engaging websites.</p>
+            <button
+              onClick={downloadCV}
+              className="bg-transparent border border-neon hover:border-1 hover:opacity-70 transition-all ease-linear text-white font-bold py-2 px-4 rounded"
+            >
+              Download My Resume
+            </button>
           </div>
         </div>
           <ComputersCanvas />
